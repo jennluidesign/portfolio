c0c5decff295ba82b3cb915e95d3e110c362c9ab
jlui85 <83372669+jlui85@users.noreply.github.com>
2021-06-12 13:24:07 -0400

    Clear w3c errors and update contacts

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 console.log("Hello world! Thanks for stopping by my portfolio site. Take a look around and let me know what you think - I'd love to hear from you!")
 
-function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
 }
